e96965f17eef795bcba1ed97e85e927d96522251
Calvin Yu <me@sourcebender.com>
2020-01-18 22:01:05 -0500

    Support SSH keys

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,18 @@ if [[ -z "${1:-}" ]]; then
   exit 1
 fi
 
+
+if [[ -n "$SSH_PRIVATE_KEY" ]]
+then
+  mkdir -p /root/.ssh
+  echo "$SSH_PRIVATE_KEY" > /root/.ssh/id_rsa
+  chmod 600 /root/.ssh/id_rsa
+fi
+
+mkdir -p ~/.ssh
+cp /root/.ssh/* ~/.ssh/ 2> /dev/null || true 
+
+
 REMOTE_URL=$1
 REMOTE=upstream-$(date +%s)
 
